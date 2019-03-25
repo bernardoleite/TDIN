ac06aee2178e1f459b8ae511bc324b1fc2900ee5
bernardoleite <up201404464@gcloud.fe.up.pt>
2019-03-25 12:24:23 +0000

    Some classes added

---
+++ Restaurant/Common/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common
+{
+    [Serializable]
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public enum Type { Food, Drink };
+    }
+}
---
+++ Restaurant/Common/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    [Serializable]
+    public class Table
+    {
+
+        public int Id;
+        public enum State { Waiting, Done, Closed }
+    }
+}
