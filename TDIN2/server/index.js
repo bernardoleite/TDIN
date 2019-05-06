88af366af85abe2f1f25bd0b2ba18dbae9c17dc0
bernardoleite <up201404464@gcloud.fe.up.pt>
2019-05-06 22:29:14 +0100

    Update repo

---
+++ TDIN2/server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require ('path');
+const exphbs = require('express-handlebars');
+
+// Database
+const db = require('./config/database')
+
+// Test db
+db.authenticate()
+    .then(() => console.log('Database connected...'))
+    .catch(err => console.log('Error:' + err))
+
+const app = express();
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Set static Folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Store API Routes
+app.use('/api/store', require('./routes/api/store'));
+
+//Homepage Route
+// app.get('/', (req,res) => res.render('index'));
+
+//Members API Routes
+// app.use('/api/members', require('./routes/api/members'));
+
+// app.get ('/index', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// app.get ('/about', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ TDIN2/server/routes/api/store.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../config/database');
+const Client = require('../../models/Client');
+
+// Get Client list
+router.get('/', (req, res) => 
+  Client.findAll()
+    .then(clients => {
+        console.log(clients);
+        res.sendStatus(200);
+      })
+    .catch(err => console.log(err)));
+
+module.exports = router;
