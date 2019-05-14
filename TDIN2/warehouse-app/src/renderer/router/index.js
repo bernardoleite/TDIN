11192bb8a136082d289164cfda22fa31d49b855f
julietafrade97 <up201506530@fe.up.pt>
2019-05-14 17:56:30 +0100

    created warehouse-app

---
+++ TDIN2/warehouse-app/src/renderer/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    /*{
+      path: '/',
+      name: 'landing-page',
+      component: require('@/components/LandingPage').default
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }*/
+  ]
+})
