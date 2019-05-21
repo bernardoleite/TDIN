6e38383e4545844eab5bee4a536d4d14a69e4488
bernardoleite <up201404464@gcloud.fe.up.pt>
2019-05-21 17:11:19 +0100

    changed to 'waiting'

--- TDIN2/warehouse-server-db/src/bootstrap.js
+++ TDIN2/warehouse-server-db/src/bootstrap.js
@@ -9,20 +9,20 @@ module.exports = async () => {
         orderId: 1,
         bookTitle: 'IT - A Coisa',
         quantity: 7,
-        state: 'waiting',
+        state: 'pending',
     }).catch(errHandler);
 
     const request2 = await Request.create({ 
         orderId: 8,
         bookTitle: 'The Girl With the Dragon Tattoo',
         quantity: 2,
-        state: 'waiting',
+        state: 'pending',
     }).catch(errHandler);
 
     const request3 = await Request.create({ 
         orderId: 7,
         bookTitle: 'The Girl Who Played With Fire',
         quantity: 2,
-        state: 'waiting',
+        state: 'pending',
     }).catch(errHandler);
 }
