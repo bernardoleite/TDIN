d6f62c413b68a24383cb244f6091947384688f7b
julietafrade97 <up201506530@fe.up.pt>
2019-05-21 20:15:20 +0100

    Little fix sales

--- TDIN2/store-server-db/routes/api/store.js
+++ TDIN2/store-server-db/routes/api/store.js
@@ -181,7 +181,7 @@ router.get('/getOrdersByEmail', ensureAuthenticated, (req, res) => {
 });
 
 router.get('/getSales', (req, res) => {
-  let sql = `SELECT * FROM orders O INNER JOIN books B ON O.bookId = B.id WHERE state = 'sold'`;
+  let sql = `SELECT O.id, O.clientEmail, B.title, B.unitprice, O.quantity, O.totalPrice FROM orders O INNER JOIN books B ON B.id = O.bookId WHERE O.state = 'sold'`;
   db.query(sql, { type: Sequelize.QueryTypes.SELECT }, () => {})
   .then(rows => {
     res.send(rows);
